2645c9309dea38438d5fa98bb3f40ca519255421
hyfly233 <hyfly233@outlook.com>
2023-10-20 16:20:31 +0800

    commit
    commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -11,6 +11,12 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.attribute.PosixFilePermission;
+import java.util.HashSet;
+import java.util.Set;
 
 @SpringBootApplication
 @Log4j2
@@ -24,7 +30,7 @@ public class DemoApplication {
     void onApplicationStart() throws IOException {
         log.info("-------- 复制 terraform 插件到./plugins 开始 --------");
 
-        ClassPathResource pluginsResource = new ClassPathResource("plugins/plugins.zip");
+        ClassPathResource pluginsResource = new ClassPathResource("plugins.zip");
         InputStream inputStream = pluginsResource.getInputStream();
 
         ZipUtil.unzip(inputStream, new File("./"), StandardCharsets.UTF_8);
@@ -33,16 +39,33 @@ public class DemoApplication {
         if (!file.exists() || FileUtil.isEmpty(file)) {
             log.error("-------- 复制 terraform 插件到./plugins 失败 --------");
         } else {
+            this.setFileExecutable(file);
+            log.info("-------- 复制 terraform 插件到./plugins 结束 --------");
+        }
+    }
+
+    void setFileExecutable(File file) {
+        if (file.isDirectory()) {
             File[] listFiles = file.listFiles();
             if (listFiles != null) {
                 for (File listFile : listFiles) {
-                    if (listFile.isDirectory()) {
-                        continue;
-                    }
-                    listFile.setExecutable(true);
+                    setFileExecutable(listFile);
                 }
             }
-            log.info("-------- 复制 terraform 插件到./plugins 结束 --------");
+        } else {
+            Set<PosixFilePermission> perms = new HashSet<>();
+            perms.add(PosixFilePermission.OWNER_READ);
+            perms.add(PosixFilePermission.OWNER_WRITE);
+            perms.add(PosixFilePermission.OWNER_EXECUTE);
+            perms.add(PosixFilePermission.GROUP_READ);
+            perms.add(PosixFilePermission.GROUP_WRITE);
+            perms.add(PosixFilePermission.GROUP_EXECUTE);
+            try {
+                Path path = Paths.get(file.getAbsolutePath());
+                Files.setPosixFilePermissions(path, perms);
+            } catch (Exception e) {
+                log.error("设置文件 (" + file.getName() + ") 权限失败", e);
+            }
         }
     }
 }
